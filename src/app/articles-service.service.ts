3ceab16f2d206c271ff25f2e29a14819da825629
Omar Calderon <omar.calderon@uabc.edu.mx>
2024-07-20 12:32:54 -0700

    add search input section and bootstrap generic navbar

--- src/app/articles-service.service.ts
+++ src/app/articles-service.service.ts
@@ -10,56 +10,56 @@ export class ArticlesServiceService {
   db = {articles:[
     {
       "id": 1,
-      "title": "Post 1",
+      "title": "Keeping the dream alive by traveling the world.",
       "description": "Integrate the lastes technologies with an innovative platform",
       "author": "ALIVE COOPER",
       "date": '2024-07-02'
     },
     {
       "id": 2,
-      "title": "Post 2",
+      "title": "Keeping the dream alive by traveling the world.",
       "description": "Integrate the lastes technologies with an innovative platform",
       "author": "ALIVE COOPER",
       "date": '2024-07-02'
     },
     {
       "id": 3,
-      "title": "Post 3",
+      "title": "Keeping the dream alive by traveling the world.",
       "description": "Integrate the lastes technologies with an innovative platform",
       "author": "ALIVE COOPER",
       "date": '2024-07-02'
     },
     {
       "id": 4,
-      "title": "Post 1",
+      "title": "Keeping the dream alive by traveling the world.",
       "description": "Integrate the lastes technologies with an innovative platform",
       "author": "ALIVE COOPER",
       "date": '2024-07-02'
     },
     {
       "id": 5,
-      "title": "Post 2",
+      "title": "Keeping the dream alive by traveling the world.",
       "description": "Integrate the lastes technologies with an innovative platform",
       "author": "ALIVE COOPER",
       "date": '2024-07-02'
     },
     {
       "id": 6,
-      "title": "Post 1",
+      "title": "Keeping the dream alive by traveling the world.",
       "description": "Integrate the lastes technologies with an innovative platform",
       "author": "ALIVE COOPER",
       "date": '2024-07-02'
     },
     {
       "id": 7,
-      "title": "Post 2",
+      "title": "Keeping the dream alive by traveling the world.",
       "description": "Integrate the lastes technologies with an innovative platform",
       "author": "ALIVE COOPER",
       "date": '2024-07-02'
     },
     {
       "id": 8,
-      "title": "Post 3",
+      "title": "Keeping the dream alive by traveling the world.",
       "description": "Integrate the lastes technologies with an innovative platform",
       "author": "ALIVE COOPER",
       "date": '2024-07-02'
